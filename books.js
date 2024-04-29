c723b6c943764f2d71e26afac46a71f3e987637e
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-04-29 11:40:21 -0700

    fun

---
+++ books.js
@@ -0,0 +1,16 @@
+let openbtn=document.getElementById('open-btn');
+let modalContainer=document.getElementById('modal-container');
+let closeBtn=document.getElementById('close-btn');
+
+//Event listeners
+openbtn.addEventListener('click',function(){
+  modalContainer.style.display='block'  ;
+});
+closeBtn.addEventListener('click',function(){
+    modalContainer.style.display='none'
+});
+window.addEventListener('click',function(e){
+    if(e.target===modalContainer){
+        modalContainer.style.display='none'
+    }
+});
